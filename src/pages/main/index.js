29cbaf6a092e7fe5dc253ea057a0a980d7020412
Wali Queiroz <wsantos077@gmail.com>
2019-12-10 10:32:41 -0300

    Mapa renderizado corretamente

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import MapboxGL from "@react-native-mapbox-gl/maps";
+import PropTypes from 'prop-types';
 
-// import styles from './styles';
+import { StatusBar } from 'react-native';
 
-const Main = () => (<MapboxGL.MapView
-    centerCoordinate={[-49.6446024, -27.2108001]}
-    style={{ flex: 1 }}
-    styleURL={MapboxGL.StyleURL.Dark}
-/>);
+
+import { Container } from './styles';
+
+const Main = () => (
+    <Container>
+        <StatusBar barStyle="light-content" />
+        <MapboxGL.MapView
+            centerCoordinate={[-49.6446024, -27.2108001]}
+            style={{ flex: 1 }}
+            styleURL={MapboxGL.StyleURL.Dark}
+        />
+    </Container>
+);
+
+Main.navigationOptions = {
+    header: null,
+};
 
 export default Main;
---
+++ src/pages/main/styles.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+const Container = styled.View`
+  flex: 1;
+`;
+export { Container };
